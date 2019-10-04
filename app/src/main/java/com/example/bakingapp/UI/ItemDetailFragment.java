c5d522c82dacacf0783d17fac18f9cea69c93865
Alaa <alaa.alzahrani88@gmail.com>
2019-10-04 18:32:02 +0300

    adding ExoPlayer for videos

--- app/src/main/java/com/example/bakingapp/UI/ItemDetailFragment.java
+++ app/src/main/java/com/example/bakingapp/UI/ItemDetailFragment.java
@@ -1,11 +1,27 @@
 package com.example.bakingapp.UI;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.bakingapp.Models.RecipesSteps;
 import com.example.bakingapp.R;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import androidx.fragment.app.Fragment;
@@ -16,8 +32,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.Objects;
-
 /**
  * A fragment representing a single Item detail screen.
  * This fragment is either contained in a {@link StepsActivity}
@@ -26,15 +40,12 @@ import java.util.Objects;
  */
 public class ItemDetailFragment extends Fragment {
     private static final String TAG = ItemDetailFragment.class.getSimpleName();
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy sDescription this fragment is presenting.
-     */
+    Context context;
+    private PlayerView playerView;
+    private SimpleExoPlayer simpleExoPlayer;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -64,17 +75,50 @@ public class ItemDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
-
+        context = container.getContext();
         TextView instructionTextView = rootView.findViewById(R.id.instruction);
+        playerView = rootView.findViewById(R.id.playerView);
 
-        if(getArguments().containsKey("step")){
+        if (getArguments().containsKey("step")) {
             String step = getArguments().getString("step");
             instructionTextView.setText(step);
-        }else {
+        } else {
             Intent intent = getActivity().getIntent();
             RecipesSteps steps = intent.getParcelableExtra("steps");
             instructionTextView.setText(steps.getsInstructions());
+            initializePlayer(Uri.parse(steps.getsVideoUrl()));
         }
         return rootView;
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if (simpleExoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+            // Bind the player to the view.
+            playerView.setPlayer(simpleExoPlayer);
+            // Produces DataSource instances through which media data is loaded.
+            String userAgent = Util.getUserAgent(context, "Baking App");
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context, userAgent);
+            // This is the MediaSource representing the media to be played.
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, dataSourceFactory,
+                    new DefaultExtractorsFactory(), null, null);
+
+            // Prepare the player with the source.
+            simpleExoPlayer.prepare(mediaSource);
+            simpleExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        simpleExoPlayer.stop();
+        simpleExoPlayer.release();
+        simpleExoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 }
